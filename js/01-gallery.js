57003bd907bbf8d069b138756c30af1cfd8a6407
KacperTrybus <117526502+KacperTrybus@users.noreply.github.com>
2023-09-04 01:09:30 +0200

    task1 poprawiony wg wzoru task2 start

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,15 +6,26 @@ const galleryList = document.querySelector(".gallery");
 
 galleryItems.forEach((image) => {
   const newLiItem = document.createElement("li");
+  const newDiv = document.createElement("div");
+  const newLink = document.createElement("a");
   const newImage = document.createElement("img");
+  newImage.className = "gallery__image";
+  newImage.src = image.preview;
+  newImage.setAttribute("data-source", `${image.original}`);
+  newImage.alt = "Image description";
 
-  newImage.src = image.preview; // src dla img
-  newLiItem.append(newImage); //img do li
-  galleryList.append(newLiItem); //
-  newLiItem.classList.add("gallery__item", "gallery__link");
+  galleryList.append(newLiItem); // li do ul
+
+  newLiItem.append(newDiv); //div do li
+  newDiv.classList.add("gallery__item");
+
+  newDiv.append(newLink); //a do div
+  newLink.classList.add("gallery__link");
+
+  newLink.append(newImage); //img do a
   newImage.classList.add("gallery__image");
 
-  newLiItem.addEventListener("click", displayOriginalImg);
+  newDiv.addEventListener("click", displayOriginalImg);
 
   function displayOriginalImg() {
     const bigImage = basicLightbox.create(`<img src="${image.original}">`);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,4 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
