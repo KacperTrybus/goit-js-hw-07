5d92a77406f3358cd0073e78f511ac3abf069f7f
KacperTrybus <117526502+KacperTrybus@users.noreply.github.com>
2023-09-03 19:59:54 +0200

    classes for gallery and append

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,17 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+galleryItems.forEach((image) => {
+  const newLiItem = document.createElement("li");
+  const newImage = document.createElement("img");
+
+  newImage.src = image.preview; // src dla img
+  newLiItem.append(newImage); //img do li
+  galleryList.append(newLiItem); //
+  newLiItem.classList.add("gallery__item");
+  newImage.classList.add("gallery__image");
+});
